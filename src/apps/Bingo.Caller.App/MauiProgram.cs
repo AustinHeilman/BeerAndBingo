2ead6b543b26650ac059379d7dd5a67e2c0ac4db
Austin Heilman <AustinHeilman@gmail.com>
2025-06-21 16:31:38 -0700

    Organized / set images

--- src/apps/Bingo.Caller.App/MauiProgram.cs
+++ src/apps/Bingo.Caller.App/MauiProgram.cs
@@ -16,7 +16,7 @@ namespace Bingo.Caller.App
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
