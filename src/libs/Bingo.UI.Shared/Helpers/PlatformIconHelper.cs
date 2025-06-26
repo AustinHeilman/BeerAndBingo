22224d10f7ea83cf7db9a1bf8355f93793243920
Austin Heilman <AustinHeilman@gmail.com>
2025-06-25 21:14:01 -0700

    Added icon image and button for the UI and a helper class for windows vs mobile image types

---
+++ src/libs/Bingo.UI.Shared/Helpers/PlatformIconHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Bingo.UI.Shared.Helpers;
+
+public static class PlatformIconHelper
+{
+	public static ImageSource Get(string baseName)
+	{
+#if WINDOWS
+        return ImageSource.FromFile($"{baseName}_win.png");
+#else
+		return ImageSource.FromFile($"{baseName}.svg");
+#endif
+	}
+}
