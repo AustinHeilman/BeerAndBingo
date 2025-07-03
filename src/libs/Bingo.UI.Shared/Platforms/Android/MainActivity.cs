f9a24c76a1995eb99f289fc6a7c104f492443314
Austin Heilman <AustinHeilman@gmail.com>
2025-07-02 23:18:18 -0700

    Added screen keep alive

---
+++ src/libs/Bingo.UI.Shared/Platforms/Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+
+namespace Bingo.UI.Shared;
+
+[Activity(
+	Label = "Bingo",
+	Theme = "@style/Maui.SplashTheme",
+	MainLauncher = true,
+	ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+public class MainActivity : MauiAppCompatActivity
+{
+	protected override void OnCreate(Bundle? savedInstanceState)
+	{
+		base.OnCreate(savedInstanceState);
+
+		// Keep the screen awake during gameplay
+		Window?.AddFlags(WindowManagerFlags.KeepScreenOn);
+	}
+}
--- src/libs/Bingo.UI.Shared/Platforms/Android/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Bingo.UI.Shared
-{
-	// All the code in this file is only included on Android.
-	public class PlatformClass1
-	{
-	}
-}
