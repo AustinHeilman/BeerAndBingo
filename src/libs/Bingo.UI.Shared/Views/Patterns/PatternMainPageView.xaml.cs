a6254644a8e839e5bc7284265cfe853dc572661d
Austin Heilman <AustinHeilman@gmail.com>
2025-07-03 13:26:52 -0700

    Pattern main page view shows correctly. Closes correctly

--- src/libs/Bingo.UI.Shared/Views/Patterns/PatternMainPageView.xaml.cs
+++ src/libs/Bingo.UI.Shared/Views/Patterns/PatternMainPageView.xaml.cs
@@ -4,17 +4,8 @@ namespace Bingo.UI.Shared.Views.Patterns;
 
 public partial class PatternMainPageView : ContentView
 {
-	public ICommand CloseCommand { get; }
-
 	public PatternMainPageView()
-	{
-		InitializeComponent();
-
-		CloseCommand = new Command(async () =>
-		{
-			await Navigation.PopModalAsync(); // Dismiss the modal
-		});
-
-		BindingContext = new { page = this };
+	{		
+		InitializeComponent();		
 	}
 }
