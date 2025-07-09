ece0ce45f9d9d36ca8cf3eb9c021555365aafc64
Austin Heilman <AustinHeilman@gmail.com>
2025-07-09 00:27:40 -0700

    x

---
+++ src/libs/Bingo.UI.Shared/Converters/SelectedPatternToFontConverter.cs
@@ -0,0 +1,13 @@
+﻿using Bingo.ViewModel.Patterns;
+using System.Globalization;
+
+public class SelectedPatternToFontConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		var selected = (parameter as LoadPatternView)?.BindingContext as LoadPatternViewModel;
+		return selected?.SelectedPattern == value ? FontAttributes.Bold : FontAttributes.None;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
---
+++ src/libs/Bingo.UI.Shared/Converters/SelectedPatternToStrokeConverter.cs
@@ -0,0 +1,13 @@
+﻿using Bingo.ViewModel.Patterns;
+using System.Globalization;
+
+public class SelectedPatternToStrokeConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		var selected = (parameter as LoadPatternView)?.BindingContext as LoadPatternViewModel;
+		return selected?.SelectedPattern == value ? Colors.Goldenrod : Colors.Gray;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
